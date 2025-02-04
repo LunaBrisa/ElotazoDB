b616d0524c6762cc27c104cea6594d8e8e4820c3
dianaochoa30 <dianaaochoa3@gmail.com>
2025-02-04 08:11:27 -0600

    cambios

--- routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/login', (req, res) => {
-    res.sendFile(__dirname + "/views/login.html");
-});
-
-export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
   res.render("carro", { title: "Carro de Compras" });
 });
 
+
+router.get('/login', (req, res) => {
+  res.sendFile(__dirname + "/views/login.ejs");
+});
 export default router;
